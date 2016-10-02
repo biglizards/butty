a7354323fe8318772c5004a282f1fbcf648347e2
itsharru <harry@yark.org.uk>
2016-10-02 12:43:17 +0100

    logging again

--- butty.py
+++ butty.py
@@ -26,7 +26,7 @@ import logging
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='/extras/discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='extras/discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
