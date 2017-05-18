74a8a2c5782f0a8ca3dc7320f4ae085971efdd22
itsharru <harry@yark.org.uk>
2017-05-18 22:56:13 +0100

    added more bits to help
    changed prefix somehow
    
    volume can only be set below 20 by the person who put the song on
    
    also not sure what I changed here, probably something to do with the prefix too

--- cogs/prefix.py
+++ cogs/prefix.py
@@ -3,20 +3,22 @@ import os
 
 class Prefix:
     def __init__(self):
-        self.prefixes = {}
         self.database = sqlite3.connect("cogs/buttybot.db")
         self.c = self.database.cursor()
 
         self.c.execute('''CREATE TABLE IF NOT EXISTS prefixes
                           (id text, prefix text)''')
+        self.prefixes = {}
+        for thing in self.c.execute("SELECT * FROM prefixes").fetchall():
+            self.prefixes[thing[0]] = thing[1]
 
-    def get_prefix(self, bot, message, check_db=True):
+    def get_prefix(self, bot, message, check_db=False):
         if not message.server:
             return '['
         if '{0.me.mention} '.format(message.server) in message.content:
             return '{0.me.mention} '.format(message.server)
         elif '{0.user.mention} '.format(bot) in message.content:
-            return '{0.user.mention} '.format(bot) # because sometimes a memtion has an ! in it for no reason
+            return '{0.user.mention} '.format(bot) # because sometimes a mention has an ! in it for no reason
 
         if check_db:
             prefix = self.c.execute("SELECT prefix FROM prefixes WHERE id=?", (message.server.id,)).fetchone()
