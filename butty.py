aaf21993ca778346225d47da786ecc5946a28b01
biglizard <biglizard@fuckme.moe>
2017-06-25 19:55:57 +0100

    Added secret file support, cleaned up butty.py and misc.py

--- butty.py
+++ butty.py
@@ -1,42 +1,50 @@
 #!/usr/bin/env python3
-import sqlite3
-import logging
-import os
 import traceback
 import time
+from collections import defaultdict
+from functools import wraps
 
 import discord
 from discord.ext import commands
 import cogs.prefix
 
-real_path = os.path.dirname(os.path.realpath(__file__)) + "/"
-os.chdir(real_path)
+# setup for "secret" (server specific) stuff
+
+
+def event(self, coro):
+    @wraps(coro)
+    async def wrapper(*args, **kwargs):
+        func = self.secrets.get(coro.__name__, [])
+        for command in func:
+            await command(*args, **kwargs)
+        await coro(*args, **kwargs)
+    return self.event_dec(wrapper)
+
+
+def secret(self, func):
+    self.secrets[func.__name__].append(func)
+
+commands.Bot.event_dec = commands.Bot.event
+commands.Bot.secret = secret
+commands.Bot.event = event
+
 
 prefix = cogs.prefix.Prefix()
 
-database = sqlite3.connect("cogs/buttybot.db")
-c = database.cursor()
-
-c.execute('''CREATE TABLE IF NOT EXISTS prefixes
-             (id text, prefix text)''')
-
 description = '''Butty. All you need, and more, less some things you need'''
 bot = commands.Bot(command_prefix=prefix.get_prefix, description=description)
+
 bot.voice_reload_cache = None
-bot.remove_command("help")
 bot.startup_time = time.time()
+bot.secrets = defaultdict(list)
+
+bot.remove_command("help")  # TODO: wtf harru why is this here
 
 # add cogs here after putting them in cogs folder (format cogs.<name> of file without extension>)
-startup_extensions = ["cogs.reminders", "cogs.voice", "cogs.misc", "cogs.ascii", "cogs.help"]
-
-
-logger = logging.getLogger('discord')
-logger.setLevel(logging.INFO)
-if not os.path.exists('extras/metalogs'):
-    os.makedirs('extras/metalogs')
-handler = logging.FileHandler(filename='extras/metalogs/discord.log', encoding='utf-8', mode='a')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+startup_extensions = ["cogs.reminders", "cogs.voice", "cogs.misc", "cogs.ascii", "cogs.help", "cogs.secret"]
+# TODO: remove cogs.secret from startup?
+# I don't want any differences between git and live version
+# i guess just don't sync secret.py?
 
 
 @bot.event
@@ -47,7 +55,7 @@ async def on_command_error(exception, context):
     message = context.message
     tb = ''.join(traceback.format_exception(type(exception), exception, exception.__traceback__))
 
-    reply =  "COMMAND **{}** IN **{}** ({})".format(message.content, message.server.name, message.server.id)
+    reply = "COMMAND **{}** IN **{}** ({})".format(message.content, message.server.name, message.server.id)
     reply += "\n```py\n{}```".format(tb[len(reply)-1988:])
     await bot.send_message(discord.Object('259634295256121345'), reply)
 
@@ -58,7 +66,9 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
     await bot.change_presence(game=discord.Game(name='"[help" for help'))
+
     for extension in startup_extensions:
         try:
             bot.load_extension(extension)
@@ -69,15 +79,10 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.server and message.server.id == '204621105720328193' and ('nsfw' in message.content or 'NSFW' in message.content):
-        await bot.send_message(message.channel, "(not safe for women)")
-
     await bot.process_commands(message)
 
-    if message.content.startswith('['):
-        await bot.send_message(discord.Object('237608005166825474'), "**{}** ({})\n**{}** ({})\n{}".format(message.server.name, message.server.id, message.author.name, message.author.id, message.content))
 
-
+# defined here so it can't be accidentally unloaded
 @bot.command(name="reload", hidden=True, pass_context=True)
 async def reload_module(ctx, module):
     if ctx.message.author.id == '135483608491229184' or ctx.message.author.id == '135496683009081345':
@@ -90,4 +95,4 @@ try:
         token = Token.read()
         bot.run(token)
 except FileNotFoundError:
-    print("token not found\nplease create a file called \"token\" in the \"extras\" folder and put the token in that")
+    print('token not found\nplease create a file called "token" in the "extras" folder and put the token in that')
