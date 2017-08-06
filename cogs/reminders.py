cfa00e61a81afa82cfc68f2a17f324adb458ed56
itsharru <harry@yark.org.uk>
2017-08-06 19:48:38 +0100

    possibly something to butty.py
    rewrote ascii to use d.py rewrite
    
    pushing logs because its broken

--- cogs/reminders.py
+++ cogs/reminders.py
@@ -14,9 +14,9 @@ class Reminders:
 
     def __init__(self, bot):
         self.bot = bot
-        self.database = sqlite3.connect("cogs/buttybot.db")
+        self.database = sqlite3.connect("cogs/reminders.db")
         self.cursor = self.database.cursor()
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS alert
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS alerts
                           (user, channel, time, message, repeat, id)''')
         self.loop = self.bot.loop.create_task(self.reminder_loop())
 
@@ -24,7 +24,7 @@ class Reminders:
         while not self.bot.is_closed:
             now = datetime.now(timezone('UTC'))
             now = format(now, '%Y-%m-%d %H:%M:%S')
-            alerts = self.cursor.execute("SELECT user, channel, message FROM alert WHERE time < ?", (now,)).fetchall()
+            alerts = self.cursor.execute("SELECT user, channel, message FROM alerts WHERE time < ?", (now,)).fetchall()
 
             for user_id, channel, message in alerts:
                 try:
@@ -32,7 +32,7 @@ class Reminders:
                 except:
                     pass
 
-            self.cursor.execute("DELETE FROM alert WHERE time < ?", (now,))
+            self.cursor.execute("DELETE FROM alerts WHERE time < ?", (now,))
             self.database.commit()
             await asyncio.sleep(5)
 
@@ -54,10 +54,10 @@ class Reminders:
         message = " ".join(context.message.content.split(" ")[2:])
         msg = message.split(",", 1)
         cal = parsedatetime.Calendar()
-        alertid = len(self.cursor.execute("SELECT * FROM alert WHERE user=?", (context.message.author.id,)).fetchall()) + 1
+        alertid = len(self.cursor.execute("SELECT * FROM alerts WHERE user=?", (context.message.author.id,)).fetchall()) + 1
         dontusemodulesasvariablenames = cal.parse(msg[0], datetime.now(timezone('UTC')))
         alert_time = time.strftime('%Y-%m-%d %H:%M:%S', dontusemodulesasvariablenames[0])
-        self.cursor.execute("INSERT INTO alert VALUES(?, ?, ?, ?, ?, ?)", (context.message.author.id, context.message.channel.id, alert_time, msg[1], "no", alertid))
+        self.cursor.execute("INSERT INTO alerts VALUES(?, ?, ?, ?, ?, ?)", (context.message.author.id, context.message.channel.id, alert_time, msg[1], "no", alertid))
         self.database.commit()
         await self.bot.say("Reminder set for " +  alert_time + " UTC")
 
@@ -67,27 +67,27 @@ class Reminders:
         ids = int(args[2])
         moveup = []
         user = context.message.author.id
-        removed = (self.cursor.execute("SELECT message FROM alert WHERE user=? AND id=?", (user, ids))).fetchall()
-        self.cursor.execute("DELETE FROM alert WHERE user=? AND id=?", (user, ids))
-        newid = (self.cursor.execute("SELECT id FROM alert WHERE id=?", (user,))).fetchall()
+        removed = (self.cursor.execute("SELECT message FROM alerts WHERE user=? AND id=?", (user, ids))).fetchall()
+        self.cursor.execute("DELETE FROM alerts WHERE user=? AND id=?", (user, ids))
+        newid = (self.cursor.execute("SELECT id FROM alerts WHERE id=?", (user,))).fetchall()
         for x in range(0, len(newid)):
             if newid[x][0] > ids:
                 moveup.append(newid[x][0])
         for x in range(0, len(moveup)):
-            self.cursor.execute("UPDATE alert SET id=? WHERE id=? AND user=?", ((moveup[x] - 1), moveup[x], user))
+            self.cursor.execute("UPDATE alerts SET id=? WHERE id=? AND user=?", ((moveup[x] - 1), moveup[x], user))
         self.database.commit()
         await self.bot.say("Deleted **" + removed[0][0] + "** from your reminder list")
 
     @reminder.command(aliases=['c'], pass_context=True)
     async def clear(self, context):
         user = context.message.author.id
-        self.cursor.execute("DELETE FROM alert WHERE user=?", (user,))
+        self.cursor.execute("DELETE FROM alerts WHERE user=?", (user,))
         self.database.commit()
         await self.bot.say("Your reminders have been cleared")
 
     @reminder.command(aliases=['s', 'l'], pass_context=True)
     async def show(self, context):
-        reminder_list = self.cursor.execute("SELECT message FROM alert WHERE user=?", (context.message.author.id,)).fetchall()
+        reminder_list = self.cursor.execute("SELECT message FROM alerts WHERE user=?", (context.message.author.id,)).fetchall()
         x = 0
         reply = ''
         for reminder in reminder_list:
